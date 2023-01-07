6f8ee498faad5a1f9aa8d83367915d6e48f67703
Karina Valens <karyvalens@yahoo.com>
2023-01-07 22:18:00 +0100

    giving format to the count down

--- components/booking/Countdown.js
+++ components/booking/Countdown.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useContext, useRef} from 'react'
 import { AppContext } from '../context/AppContext'
 
+const formatTime= (time)=>{
+  let minutes  = Math.floor(time / 60)
+  let seconds  = Math.floor(time - minutes * 60)
 
+  if(minutes <= 10) minutes ="0" + minutes;
+  if(seconds <= 10) seconds ="0" + minutes;
+
+  return minutes + ":" + seconds
+}
 
 export default function Countdown() {
 
@@ -30,7 +38,7 @@ if (countdown <= 0 ){
 console.log("countdown",countdown)
 
   return (
-    <h3 className='accent3'>Your reservation expires in <span className='accent2'>{countdown}s</span> </h3>
+    <h3 className='accent3'>Your reservation expires in <span className='accent2'>{formatTime(countdown)}s</span> </h3>
   )
 }
 
